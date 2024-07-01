f66a304cb7befabd510122a2df56dd1e36415923
NGUEPKAP <115540737+Ulrichnk@users.noreply.github.com>
2024-07-01 13:13:16 +0200

    refactor: Add lazy loading for login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,26 @@
 import { Suspense } from "react";
 import LoginForm from "../components/login-form";
 
+const LoginPage = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <Suspense>
+        <LoginForm />
+      </Suspense>
+    </div>
+  );
+};
+
+export default LoginPage;
+
+import { Suspense } from "react";
+import LoginForm from "../components/login-form";
+
 export default function LoginPage () {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <LoginForm />
+      <Suspense>      <LoginForm />
+</Suspense>
     </div>
   );
 };
