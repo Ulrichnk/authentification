2fe1daa1307fc2678bf0955d3bdfc3b16edb362c
NGUEPKAP <115540737+Ulrichnk@users.noreply.github.com>
2024-07-01 12:51:20 +0200

    refactor: Add lazy loading for login and register forms

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import LoginForm from "../components/login-form";
 
 const LoginPage = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 };
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import RegisterForm from '../components/register-form'
+import { Suspense } from "react";
+import RegisterForm from "../components/register-form";
 
 const RegisterPage = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <RegisterForm/>
+      <Suspense>
+        <RegisterForm />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
