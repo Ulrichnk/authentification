a083a3b4d250aa8938955e39c1d32d5fe59acb8e
NGUEPKAP <115540737+Ulrichnk@users.noreply.github.com>
2024-07-03 09:53:38 +0200

    refactor: Enable lazy loading for login page and update login code consistently

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import FormError from "../auth/components/form-error";
 
 const DashboardPages = async () => {
@@ -10,7 +11,20 @@ const DashboardPages = async () => {
       </div>
     );
   }
-  return <div>dashboard</div>;
+  return (
+    <div>
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default DashboardPages;
